71a7f4527d21ea6915cae547809be04d9901f96e
miguelferra <miguelferra4@gmail.com>
2020-03-19 18:29:21 -0700

    Segunda Version (Implementacion de bakckend)

---
+++ app/src/main/java/Task.kt
@@ -0,0 +1,2 @@
+package ferra.miguel
+data class Task(var title:String, var days:ArrayList<String>,var time:String)
--- app/src/main/java/ferra/miguel/digimind/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/ferra/miguel/digimind/ui/dashboard/DashboardFragment.kt
@@ -1,14 +1,23 @@
 package ferra.miguel.digimind.ui.dashboard
 
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import ferra.miguel.Task
 import ferra.miguel.digimind.R
+import ferra.miguel.digimind.ui.home.HomeFragment
+import kotlinx.android.synthetic.main.fragment_dashboard.*
+import kotlinx.android.synthetic.main.fragment_dashboard.view.*
+import kotlinx.android.synthetic.main.fragment_dashboard.view.btn_time
+import java.text.SimpleDateFormat
+import java.util.*
 
 class DashboardFragment : Fragment() {
 
@@ -22,10 +31,49 @@ class DashboardFragment : Fragment() {
         dashboardViewModel =
             ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        /*val textView: TextView = root.findViewById(R.id.text_dashboard)*/
-        dashboardViewModel.text.observe(this, Observer {
-            /* textView.text = it */
-        })
+        root.btn_time.setOnClickListener {
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+
+                btn_time.text = SimpleDateFormat("HH:mm").format(cal.time)
+            }
+            TimePickerDialog(
+                root.context,
+                timeSetListener,
+                cal.get(Calendar.HOUR_OF_DAY),
+                cal.get(Calendar.MINUTE),
+                true
+            ).show()
+
+        }
+        root.btn_save.setOnClickListener {
+            var title = et_task.text.toString()
+            var time = btn_time.text.toString()
+
+            var days = ArrayList<String>()
+
+            if (checkMonday.isChecked)
+                days.add("Monday")
+            if (checkTuesday.isChecked)
+                days.add("Tuesday")
+            if (checkWednesday.isChecked)
+                days.add("Wednesday")
+            if (checkThursday.isChecked)
+                days.add("Thursday")
+            if (checkFriday.isChecked)
+                days.add("Friday")
+            if (checkSatuday.isChecked)
+                days.add("Saturday")
+            if (checkSunday.isChecked)
+                days.add("Sunday")
+
+            var task = Task(title,days,time)
+
+            HomeFragment.tasks.add(task)
+            Toast.makeText(root.context,"new task added",Toast.LENGTH_SHORT).show()
+        }
         return root
     }
 }
